5db5f395faefbf89258b96c288f4d4dbb9d5cfb1
inamuu <inamura@pepabo.com>
2016-06-28 13:44:40 +0900

    Add fortune cowsay

--- lib/oasobi/cowsay.rb
+++ lib/oasobi/cowsay.rb
@@ -1,8 +1,13 @@
 module Oasobi
   class CLI < Thor
     desc "cowsay WORDS", "Cow say what you want!!"
-    def cowsay2(word)
+    def cowsay(word)
       exec("cowsay #{word}")
+    end
+
+    desc "cowsay2", "Cow say what a random."
+    def cowsay2
+      exec("fortune | cowsay")
     end
   end
 end
