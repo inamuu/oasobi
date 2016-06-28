ae6ac1696ec43efcabe68534513ab62881537e73
inamuu <inamura@pepabo.com>
2016-06-28 13:24:27 +0900

    Fixed bug cowsay command

--- lib/oasobi/cowsay.rb
+++ lib/oasobi/cowsay.rb
@@ -1,11 +1,6 @@
 module Oasobi
   class CLI < Thor
-    desc "cowsay", "Cow say just only!!"
-    def cowsay
-      exec("cowsay")
-    end
-
-    desc "cowsay2 WORDS", "Cow say what you want!!"
+    desc "cowsay WORDS", "Cow say what you want!!"
     def cowsay2(word)
       exec("cowsay #{word}")
     end
